beb9c6ed10309c147f7cd856cffd0902d99eebaf
MeerimAlimbekova <alimbekova25m@gmail.com>
2021-06-30 19:32:14 -0400

    adding more review

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Recapping like day1");
+        System.out.println("Recapping like day1, I wanted to recap more");
 
     }
 }
